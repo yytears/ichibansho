7527f177a625297446f0957e486355f321b2f8fb
csq <caoshengqian_it@163.com>
2021-06-26 15:28:09 +0800

    bin

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ git pull
 #复制jar包
 cp $INTLOCK_DIR/jinxiu-admin/target/jinxiu-admin.jar $JAR_DIR/jar/$time.jar
 # 停止SENS
-sh $INTLOCK_DIR/jx.sh stop
+sh $INTLOCK_DIR/bin/intlocktest.sh stop
 # 设置环境变量，执行打包
 mvn clean package -Dmaven.test.skip=true
 echo "代码打包完毕！"
 # 进入打包好的SENS目录
 cd $INTLOCK_DIR
 # 运行SENS
-sh $INTLOCK_DIR/jx.sh start
+sh $INTLOCK_DIR/bin/intlocktest.sh start
 echo "部署完毕，Enjoy！"
